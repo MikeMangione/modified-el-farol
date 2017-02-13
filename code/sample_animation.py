157389d8b35b222a353a3f4cd6fb7dce0400fdeb
MikeMangione <mjbm@bu.edu>
2017-02-13 09:09:45 -0500

    switched from patch to scatter plot animation

--- code/sample_animation.py
+++ code/sample_animation.py
@@ -1,34 +1,47 @@
 #!/usr/bin/env python
 import numpy as np
+import random
 from matplotlib import pyplot as plt
 from matplotlib import animation\
+
+def step(x,y,queue,bar):
+    for y_ in range(0,len(y)):
+        if queue[y_] == 0:
+            y[y_] = (y[y_]+0.1)%10
+            if y[y_] > 6 and y_ % 2 == 0:
+                queue[y_] = 1
+        elif bar[y_] == 0:
+            if x[y_] < 6 - y_ * 0.00002 * sum(queue):
+                x[y_] += 0.1
+            if y[y_] > 4:
+                y[y_] -= 0.2
+    return x,y
+
+def update_pos(p):
+    x = [2 for i in range(0,p)]
+    y = [random.random()*10 for i in range(0,p)]
+    return x,y
+
+def animate(i):
+    plt.clf()
+    ax = plt.axes(xlim=(0, 10), ylim=(0, 10))
+    x, y = step(x_movements, y_movements, queue, bar)
+    plt.scatter(x_movements,y_movements)
+    return
+
+people = 1000
+queue = [0 for x in range(0,people)]
+bar = [0 for x in range(0,people)]
+x_movements, y_movements = update_pos(people)
 
 fig = plt.figure()
 fig.set_dpi(100)
 fig.set_size_inches(5, 5)
 
 ax = plt.axes(xlim=(0, 10), ylim=(0, 10))
-patch = [plt.Circle((1, -1), 0.1, fc='r') for x in range(0,10)]
-
-y_ = [0,1,2,3,4,5,6,7,8,9,10]
-
-def init():
-    for z in range(0,len(patch)):
-        patch[z].center = (2, 0)
-        ax.add_patch(patch[z])
-    return patch,
-
-def animate(i):
-    for z in range(0,len(patch)):
-        x, y = patch[z].center
-        x = x
-        y = y_[(i+z)%len(y_)]
-        patch[z].center = (x, y)
-    return patch,
 
 anim = animation.FuncAnimation(fig, animate,
-                               init_func=init,
                                frames = 10,
-                               interval=200,
+                               interval=1,
                                blit=False)
 plt.show()
