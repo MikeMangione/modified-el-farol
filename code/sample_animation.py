2440c81b483b602ff08170e55dcf648ac8faf546
MikeMangione <mjbm@bu.edu>
2017-04-02 19:13:04 -0400

    improve OOD and replace sqlite with txt for improved speed

--- code/sample_animation.py
+++ code/sample_animation.py
@@ -1,140 +1,123 @@
 #!/usr/bin/env python
-import sqlite3
 import numpy as np
 import sys
 import time
+import math
 import random
 import os.path
 import matplotlib.patches as patches
 from matplotlib import pyplot as plt
 from matplotlib import animation\
 
-ITERATION = 0
-people = 1000
-sqlite_file = 'el-farol_stats.sqlite'    # name of the sqlite database file
-table_names = ['bar_stats','queue_stats','street_stats']
-iteration_col, patron_col, in_bar_col, in_queue_col, in_street_col = 'iteration', 'patron_number', 'in_bar', 'in_queue', 'in_street'
-characteristics_str = 'Optimism', 'Patience', 'Extroversion'
-locations_str = 'Bar', 'Queue', 'Street'
-stats_out = ['' for x in locations_str]
-for x in range(0,len(stats_out)):
-    stats_out[x] = locations_str[x]+'\nPopulation %'
-    for y in range(0,len(characteristics_str)):
-        stats_out[x] += ':\n'+characteristics_str[y]
-characteristics = [[random.random()*people for x in range(0,people)] for x in characteristics_str]
-ind_wait = [0 for x in range(0,people)]
-timer = [0 for x in range(0,people)]
-locations = [[0 for x in range(0,people)] for x in locations_str]
-leaving_q = [0 for x in range(0,people)]
-leaving_b = [0 for x in range(0,people)]
+class Patron:
+    def __init__(self,*args):
+        self.optimism = random.randint(0,(people+1))
+        self.patience = random.randint(0,(people+1))
+        self.extroversion = random.randint(0,(people+1))
+        self.location = "Street"
+        self.x = 2
+        self.y = random.random()*10
+        self.time_entered_queue = 0
+        self.length_at_queue_start = 0
 
+    def state_string(self):
+        return str(self.optimism)+","+str(self.patience)+","+str(self.extroversion)+","+str(self.location)
+
+db = ["iteration, optimism, patience, extroversion, location"]
+ITERATION = 0
+people = int(raw_input("\nHow large is the population? "))
+patrons = [Patron() for x in range(people)]
+
+def trait_handler(trait_type, trait_value):
+    return random.randint(0,(people+1))
 #average characteristic value by location
 def avg_char_by_loc(loc_arr,char_arr):
     char_temp = 0
-    for x in range(0,people):
-        if loc_arr[x] == 1:
-            char_temp += char_arr[x]
-    return "{0:.2f}".format(char_temp/sum(loc_arr)/people)
+    for p in patrons:
+        thing = [p.optimism,p.patience,p.extroversion][char_arr]
+        if p.location == loc_arr:
+            char_temp += thing
+    if sum(1 for p in patrons if p.location == loc_arr) != 0:
+        return "{0:.2f}".format(char_temp/sum(1 for p in patrons if p.location == loc_arr)/people)
+    else:
+        return 0
 
-#execute patron behavior based on location and personality characteristics
-def step(x,y,timer):
-    bar, queue, street = locations
-    optimism, patience, extroversion = characteristics
+#execute patrons behavior based on location and personality characteristics
+def step():
     temp_time = time.time()
-    for y_ in range(0,people):
-        if queue[y_] == 0 and bar[y_] == 0 and leaving_q[y_] == 0 and leaving_b[y_] == 0:
-            x[y_], y[y_] = 1.95 + random.random()*0.1, (y[y_]+0.1)%10
-            if y[y_]+0.1 > 10:
-                optimism[y_] = random.random()*people
-            if y[y_] < 4 and y[y_] > 3.5 and sum(queue) < optimism[y_]:
-                x[y_], y[y_] = 2.4, 3.9 + random.random()*0.2
-                queue[y_] = 1
-                ind_wait[y_] = sum(queue)
-                timer[y_] = ITERATION
-        elif leaving_q[y_] == 1:
-            x[y_] = 5.65 + + random.random()*0.1
-            y[y_] -= 0.2
-            if y[y_] < 0:
-                x[y_],y[y_] = 2,0
-                patience[y_] = random.random()*people
-                leaving_q[y_] = 0
-        elif leaving_b[y_] == 1:
-            x[y_] = 6.95 + random.random()*0.1
-            y[y_] += 0.2
-            if y[y_] > 10:
-                x[y_],y[y_] = 2,0
-                extroversion[y_] = random.random()*people
-                leaving_b[y_] = 0
-        elif bar[y_] == 0 and queue[y_] == 1:
-            if x[y_] < 5.7:
-                x[y_] += 0.1
-            elif x[y_] > 5.7:
-                x[y_] = 5.7
-            y[y_] = 3.9 + random.random()*0.2
-            if x[y_] == 5.7:
-                if ITERATION - timer[y_] > ind_wait[y_]:
-                    bar[y_] = 1
-                    queue[y_], ind_wait[y_] = 0,0
-                elif ITERATION - timer[y_] > patience[y_]:
-                    leaving_q[y_] = 1
-                    y[y_] = 3.2
-                    queue[y_], ind_wait[y_] = 0,0
-        elif bar[y_] == 1:
-            if sum(bar) > extroversion[y_]:
-                leaving_b[y_] = 1
-                x[y_],y[y_] = 7,8.35
-                bar[y_] = 0
+    for y_ in patrons:
+        if y_.location == "Street" and not (y_.y < 4 and y_.y > 3.5 and sum(1 for p in patrons if p.location == "Queue") < y_.optimism):
+            y_.x, y_.y = 1.95 + random.random()*0.1, (y_.y+0.1)%10
+            #reset the patron's optimism stat if out of frame
+            if y_.y + 0.1 > 10:
+                y_.optimism = trait_handler(characteristics_str[0],y_.optimism)
+            #if the patrons exist in a certain location, and the queue is less than their patience threshold
+        elif y_.location == "Street":
+                y_.x, y_.y = 2.4, 3.9 + random.random()*0.2
+                y_.location = "Queue"
+                y_.length_at_queue_start = sum(1 for p in patrons if p.location == "Queue")
+                y_.time_entered_queue = ITERATION
+        elif y_.location == "Leaving_Queue":
+            y_.x = 5.65 + + random.random()*0.1
+            y_.y -= 0.2
+            if y_.y < 0:
+                y_.x,y_.y = 2,0
+                y_.patience = trait_handler(characteristics_str[1],y_.patience)
+                y_.location = "Street"
+        elif y_.location == "Leaving_Bar":
+            y_.x = 6.95 + random.random()*0.1
+            y_.y += 0.2
+            #reset extroversion stats when out of frame
+            if y_.y > 10:
+                y_.x,y_.y = 2,0
+                y_.extroversion = trait_handler(characteristics_str[2],y_.extroversion)
+                y_.location = "Street"
+        elif y_.location == "Queue":
+            if y_.x < 5.7:
+                y_.x += 0.05
+            elif y_.x >= 5.7:
+                y_.x = 5.7
+            y_.y = 3.9 + random.random()*0.2
+            if y_.x == 5.7:
+                if ITERATION - y_.time_entered_queue > y_.length_at_queue_start:
+                    y_.location = "Bar"
+                    y_.length_at_queue_start = 0
+                elif ITERATION - y_.time_entered_queue > y_.patience:
+                    y_.location = "Leaving_Queue"
+                    y_.y = 3.2
+                    y_.length_at_queue_start = 0
+        elif y_.location == "Bar":
+            if sum(1 for p in patrons if p.location == "Bar") > y_.extroversion:
+                y_.location = "Leaving_Bar"
+                y_.x,y_.y = 7,8.35
             else:
-                x[y_], y[y_] = 6.2 + random.random()*1.6, 3.8 + random.random()* 4
-        if (bar[y_], queue[y_]) == (0,0):
-            street[y_] = 1
-        else:
-            street[y_] = 0
+                y_.x, y_.y = 6.2 + random.random()*1.6, 3.8 + random.random()* 4
+        table_name1 = y_.location
         global ITERATION
-        if ITERATION % 10 == 0:
-            if y_ == 1:
-                print ITERATION
-            if bar[y_] == 1:
-                table_name1 = 'bar_stats'
-            elif queue[y_] == 1:
-                table_name1 = 'queue_stats'
-            elif street[y_] == 1:
-                table_name1 = 'street_stats'
-            c.execute('INSERT INTO {tn} ({it},{pt},{oc},{pc},{ec})\
-                VALUES({iter},{num},{opt},{pat},{ext})'\
-                .format(tn=table_name1, it=iteration_col,\
-                pt=patron_col, oc=characteristics_str[0], pc=characteristics_str[1],\
-                ec=characteristics_str[2],iter=ITERATION,\
-                num=y_,\
-                opt=optimism[y_]*1.0/people,pat=patience[y_]*1.0/people,ext=extroversion[y_]*1.0/people
-                ))
+        db.append(str(ITERATION)+","+y_.state_string())
     ITERATION += 1
-
-#randomly distribute starting positions of patrons on x = 2
-def init_pos(p):
-    x = [2 for i in range(0,p)]
-    y = [random.random()*10 for i in range(0,p)]
-    return x,y
 
 def animate(i):
     plt.clf()
     plt.style.use('fivethirtyeight')
     fig.suptitle('Modified El-Farol', fontsize=14, fontweight='bold')
     ax = plt.axes(xlim=(0, 10), ylim=(0, 10))
+    #hueristic box sizes for patronsmovement
     ax.add_patch(patches.Rectangle((1.8,-0.1),0.4,10.2,fill=False,linewidth=3,edgecolor='#af0000'))
     ax.add_patch(patches.Rectangle((2.3,3.6),3.6,0.8,fill=False,linewidth=3,edgecolor='#afaf55'))
     ax.add_patch(patches.Rectangle((6,3.6),2,4.4,fill=False,linewidth=3,edgecolor='#00af00'))
     ax.add_patch(patches.Rectangle((6.8,8.2),0.4,4,fill=False,linewidth=3,edgecolor='#af0000'))
     ax.add_patch(patches.Rectangle((5.5,0),0.4,3.4,fill=False,linewidth=3,edgecolor='#af0000'))
-    step(x_movements, y_movements, timer)
+    step()
     ax.set_title('Frame Number: '+str(ITERATION))
+    x_movements,y_movements = [z.x for z in patrons],[z.y for z in patrons]
     plt.scatter(x_movements,y_movements)
     global stats_out
-    if sum(locations[0]) > 0:
+    if any(p.location == "Bar" for p in patrons):
         for x in range(0,len(stats_out)):
-            stats_out[x] = locations_str[x]+'\nPopulation %:'+"{0:.2f}".format(sum(locations[x])*1.0/people)
-            for y in range(0,len(characteristics)):
-                stats_out[x] += '\n'+characteristics_str[y]+': '+avg_char_by_loc(locations[x],characteristics[y])
+            stats_out[x] = locations_str[x]+'\nPopulation %:'+"{0:.2f}".format(sum(1 for p in patrons if p.location == locations_str[x])*1.0/people)
+            for y in range(0,len(characteristics_str)):
+                stats_out[x] += '\n'+characteristics_str[y]+': '+str(avg_char_by_loc(locations_str[x],y))
     ax.text(7, 1, stats_out[0], style='italic',
         bbox={'facecolor':'green', 'alpha':0.5, 'pad':10})
     ax.text(2.8, 1, stats_out[1], style='italic',
@@ -143,18 +126,13 @@ def animate(i):
         bbox={'facecolor':'red', 'alpha':0.5, 'pad':10})
     return
 
-conn = sqlite3.connect(sqlite_file)
-c = conn.cursor()
-for x in table_names:
-    c.execute('drop table if exists {tn}'.format(tn=x))
-    c.execute('CREATE TABLE {tn} ({it} {ft},{pt} {ft},{oc} {ft},{pc} {ft},{ec} {ft}\
-        )'\
-            .format(tn=x, it=iteration_col,\
-            pt=patron_col, oc=characteristics_str[0], pc=characteristics_str[1],\
-            ec=characteristics_str[2], ft='INTEGER',\
-            ))
-
-x_movements, y_movements = init_pos(people)
+characteristics_str = 'Optimism', 'Patience', 'Extroversion'
+locations_str = 'Bar','Queue','Street',"Leaving_Queue","Leaving_Bar"
+stats_out = ['' for x in locations_str]
+for x in range(0,len(stats_out)):
+    stats_out[x] = locations_str[x]+'\nPopulation %'
+    for y in range(0,len(characteristics_str)):
+        stats_out[x] += ':\n'+characteristics_str[y]
 
 plt.style.use('fivethirtyeight')
 
@@ -167,14 +145,10 @@ anim = animation.FuncAnimation(fig, animate,
                                interval=1,
                                blit=False)
 plt.show()
-#for x in range(0,10000):
-#    animate(x)
-for x in table_names:
-    c.execute('SELECT * FROM {tn}'.format(tn=x))
-    result = c.fetchall()
-    file = open(x+".txt","w")
-    for r in result:
-        file.write(str(r).lstrip('()').rstrip(')')+'\n')
-    file.close()
-conn.commit()
-conn.close()
+
+#make .txt files for three states of patronslocation:
+#street, queue, and bar
+file = open("results.txt","w")
+for x in db:
+    file.write(x+'\n')
+file.close()
